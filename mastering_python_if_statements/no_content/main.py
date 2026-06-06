51a032f96f91aaa52396c44ec41e3337bc4ff789
way2geek11 <>
2026-06-06 15:24:41 +0000

    No content

---
+++ mastering_python_if_statements/no_content/main.py
@@ -0,0 +1,18 @@
+steps_taken = 12000
+step_goal = 10000
+calories_burned = 350
+calorie_goal = 500
+
+result_message = False
+if (steps_taken >= step_goal or calories_burned >= calorie_goal):
+    result_message = True
+print("Great job, you've reached at least one goal")
+
+#if steps_taken >= step_goal:
+    #result_message = True
+#if calories_burned >= calorie_goal:
+    #result_message = True
+
+
+#if result_message:
+    #print("Great job, you've reached at least one goal")
