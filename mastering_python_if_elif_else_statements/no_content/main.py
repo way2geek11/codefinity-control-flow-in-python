8fb887341059dadc34a6800c0cbfe8606df28fe5
way2geek11 <>
2026-06-06 16:00:51 +0000

    No content

--- mastering_python_if_elif_else_statements/no_content/main.py
+++ mastering_python_if_elif_else_statements/no_content/main.py
@@ -1,15 +1,14 @@
-workout_duration = 45
-message = ""
+exercise_time = 8  # Example value for total exercise time in hours
+title = ""
 
-# Check if the workout duration is more than 60 minutes
-if workout_duration>60:
-    message = "Excellent job! You've worked out for over an hour!"
-# Check if the workout duration is between 30 and 60 minutes
-elif 30<=workout_duration<=60:
-    message = "Great job! Keep it up!"
-# If the workout duration is less than 30 minutes
+if exercise_time>10:
+    title = "Super Achiever"
+elif 6<=exercise_time<=10:
+    title = "Hard Worker"
+elif 3>=exercise_time<6:
+    title = "Getting There"
 else:
-    message = "Try to aim for at least 30 minutes of exercise."
+    title = "Needs Improvement"
 
 # Testing
-print("Exercise feedback:", message)
+print("Your fitness level:", title)
